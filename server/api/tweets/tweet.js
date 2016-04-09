398ee71697464cae9ecf67b7463f87fd3d24c077
PeterKow <peter.kowalczyk@aurity.co>
2016-04-09 17:06:15 +0100

    Circle + search queries

--- server/api/tweets/tweet.js
+++ server/api/tweets/tweet.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Q = require('q')
+const qs = require('querystring');
 
 const Boom = require('boom');
 const Joi = require('joi');
@@ -16,31 +16,78 @@ internals.applyRoutes = function (server, next) {
 
     server.route({
         method: 'GET',
-        path: '/dbTest',
+        path: '/username',
         config: {},
         handler: getTweetsByUserName
     });
 
 
     function getTweetsByUserName (request, reply) {
+        //console.log('request', request.url)
+        //console.log('sss', qs.parse('{%22limit%22:50,%22sort%22:{%22favorite_count%22:1},%22userName%22:%22dan_abramov%22,%22favorite_count%22:10,%22retweet_count%22:10}'))
 
-        const query = {};
-        const fields = request.query.fields;
-        const sort = request.query.sort;
-        const limit = request.query.limit;
-        const page = request.query.page;
+        const query = request.query
+        const fields = query.fields;
+        const sort = query.sort;
+        const limit = parseInt(query.limit,10) || 10;
+        const page = query.page;
 
-        console.log('----', query, fields, sort, limit, page)
+        console.log('query', query)
 
+        //console.log('query', qs.parse(query))
+
+        // TODO hepijs validation!!!
+        const newQuery = {
+            'user.screen_name': query.userName.toLowerCase(),
+            "favorite_count": {
+                "$gt": parseInt(query.favoriteCount,10),
+            },
+            "retweet_count": {
+                "$gt": parseInt(query.retweetCount,10),
+            },
+        }
+
+
+//         {
+//    "_id": null, 
+//   "favorite_count": { $stdDevSamp: "$favorite_count" },
+//   "retweet_count": { $stdDevSamp: "$retweet_count" },
+  
+//   "favorite_count_avg": { $avg: "$favorite_count" },
+//   "retweet_count_avg": { $avg: "$retweet_count" }
+// }
+
+
+// agregate .aggregate({    $project: {        item: 1,        comparisonResult: {            $strcasecmp: [                "$text",                "react"             ]        }    }})
+
+
+// { "text": { $in: [/^react.*redux/i] } }
+
+
+        //console.log('query', qs.parse(query))
+        //console.log('querynewww', qs.parse('foo[bar][baz]=foobarbaz')))
+        //console.log('querynewww', qs.parse('sort%5B0%5D%5Bfavorite_count%5D=1&userName=dan_abramov&favorite_count=10&retweet_count=10',  { allowPrototypes: true } ))
+        console.log('new query', newQuery)
+        //console.log('sort', sort)
         Tweet
-            .findByUsername('dan_abramov')
-            .then( data => {
-                reply(data)
+            .pagedFind(newQuery, fields, sort, limit, page, function (err, results) {
+
+                if (err) {
+                    return reply(Boom.badGateway(err))
+                }
+
+                reply(results)
             })
-            .catch( data => {
-                console.log('---- ERROR', data)
-                reply(data)
-            })
+
+    //    Tweet
+    //        .findByUsername('dan_abramov')
+    //        .then( data => {
+    //            reply(data)
+    //        })
+    //        .catch( data => {
+    //            console.log('---- ERROR', data)
+    //            reply(data)
+    //        })
     }
 
     next();
@@ -57,4 +104,4 @@ exports.register = function (server, options, next) {
 
 exports.register.attributes = {
     name: 'tweet'
-};
+}
